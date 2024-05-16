d58190a244ba926d623d9f75dc15b0d9d344da49
NAR <yyazok@gmail.com>
2024-05-16 22:24:43 +0300

    V1.1 Beta

--- script.js
+++ script.js
@@ -1,39 +1,77 @@
 const progressBar = document.getElementById('progress');
-  let width = 0;
-  var score = 0;
+let width = 0;
+let score = 0;
+const scrbrd = document.getElementById("score");
+let intervalId;
 
-  function move() {
+// Başlatma fonksiyonu
+function startGame() {
+    document.getElementById("myPopup").style.display = "none";
+    generateNumber();
+    intervalId = setInterval(move, 50);
+}
+
+// Bitirme fonksiyonu
+function endGame() {
+    clearInterval(intervalId);
+    width = 0;
+    score = 0;
+    scrbrd.innerText = score;
+    generateNumber();
+}
+
+// Durdurma fonksiyonu
+function pauseGame() {
+    clearInterval(intervalId);
+}
+
+function move() {
     if (width >= 100) {
-      width = 0;
-      generateNumber()
-    } else {
-      width++;
-      progressBar.style.width = width + '%';
-    }
-  }
-
-  setInterval(move, 50);
-
-  function checkNumber(number) {
-    var numberToCheck = number.toString();
-    var displayValue = document.getElementById("number-display").innerText;
-    if (displayValue.includes(numberToCheck)) {
-        alert(numberToCheck + " zaten mevcut! Puan'ın:" + score);
         width = 0;
-        generateNumber()
         score = 0;
+        scrbrd.innerText = 0;
+        generateNumber();
     } else {
-        score = score + 1;
+        width++;
+        progressBar.style.width = width + '%';
+    }
+}
+
+function checkNumber(number) {
+    const numberToCheck = number.toString();
+    const displayValue = document.getElementById("number-display").innerText;
+    if (displayValue.includes(numberToCheck)) {
+        showPopup(numberToCheck + " rakamı sayını içinde var. Puan'ın:" + score, 5000)
+        width = 0;
+        score = 0;
+        scrbrd.innerText = score;
+    } else {
+        score++;
+        scrbrd.innerText = score;
         width = 0;
         generateNumber();
     }
-  }
+}
 
-  function generateNumber() {
-  var number = "";
-  for (var i = 0; i < 9; i++) {
-    var digit = Math.floor(Math.random() * 9) + 1;
-    number += digit;
+function generateNumber() {
+    let number = "";
+    for (let i = 0; i < 9; i++) {
+        const digit = Math.floor(Math.random() * 9) + 1;
+        number += digit;
+    }
+    document.getElementById("number-display").innerText = number;
+}
+
+// Popup gösterme fonksiyonu
+function showPopup(text, duration) {
+  const popup = document.getElementById("myPopup");
+  const popupContent = document.querySelector(".popup-content");
+  popupContent.innerHTML = "<p>" + text + "</p>";
+  popup.style.display = "block";
+  
+  if (duration) {
+      setTimeout(function() {
+          document.getElementById("myPopup").style.display = "none";
+      }, duration);
   }
-  document.getElementById("number-display").innerText = number;
 }
