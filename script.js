5647f789d78e5fe091280299870740e9d6be7616
NAR <yyazok@gmail.com>
2024-05-19 07:59:31 +0300

    V1.1

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const progressBar = document.getElementById('progress');
-let width = 0;
-let score = 0;
 const scrbrd = document.getElementById("score");
+const levelBox = document.getElementById("level");
+let width = 0;
+let numberLenght = 9;
+let score = 0;
 let intervalId;
 
+
 // Başlatma fonksiyonu
-function startGame() {
-    document.getElementById("myPopup").style.display = "none";
-    generateNumber();
-    intervalId = setInterval(move, 50);
+function startGame(level) {
+    scrbrd.innerText = score;
+    if (level == 0) {
+        numberLenght = 7;
+        levelBox.innerText = "kolay";
+        document.getElementById("myPopup").style.display = "none";
+        generateNumber(numberLenght);
+        intervalId = setInterval(move, 50);
+    } else if (level == 1) {
+        numberLenght = 8;
+        levelBox.innerText = "orta";
+        document.getElementById("myPopup").style.display = "none";
+        generateNumber(numberLenght);
+        intervalId = setInterval(move, 35);
+    } else if (level == 2) {
+        numberLenght = 9;
+        levelBox.innerText = "zor";
+        document.getElementById("myPopup").style.display = "none";
+        generateNumber(numberLenght);
+        intervalId = setInterval(move, 20);
+    }
 }
 
 // Bitirme fonksiyonu
 function endGame() {
     clearInterval(intervalId);
+    document.getElementById("myPopup").style.display = "block";
     width = 0;
     score = 0;
     scrbrd.innerText = score;
-    generateNumber();
+    generateNumber(numberLenght);
+    
 }
 
 // Durdurma fonksiyonu
@@ -30,7 +52,7 @@ function move() {
         width = 0;
         score = 0;
         scrbrd.innerText = 0;
-        generateNumber();
+        generateNumber(numberLenght);
     } else {
         width++;
         progressBar.style.width = width + '%';
@@ -41,37 +63,20 @@ function checkNumber(number) {
     const numberToCheck = number.toString();
     const displayValue = document.getElementById("number-display").innerText;
     if (displayValue.includes(numberToCheck)) {
-        showPopup(numberToCheck + " rakamı sayını içinde var. Puan'ın:" + score, 5000)
-        width = 0;
-        score = score - 1;
-        scrbrd.innerText = score;
+        endGame()
     } else {
         score++;
         scrbrd.innerText = score;
         width = 0;
-        generateNumber();
+        generateNumber(numberLenght);
     }
 }
 
-function generateNumber() {
+function generateNumber(length) {
     let number = "";
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < length; i++) {
         const digit = Math.floor(Math.random() * 9) + 1;
         number += digit;
     }
     document.getElementById("number-display").innerText = number;
-}
-
-// Popup gösterme fonksiyonu
-function showPopup(text, duration) {
-  const popup = document.getElementById("myPopup");
-  const popupContent = document.querySelector(".popup-content");
-  popupContent.innerHTML = "<p>" + text + "</p>";
-  popup.style.display = "block";
-  
-  if (duration) {
-      setTimeout(function() {
-          document.getElementById("myPopup").style.display = "none";
-      }, duration);
-  }
 }
